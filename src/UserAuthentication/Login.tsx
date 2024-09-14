9c4605fee98a6b584ad2b62b80f8dadabe6d296c
SALAB\dellv <salabyaham@gmail.com>
2024-09-14 15:08:34 +0600

    Login Form Redesigned

--- src/UserAuthentication/Login.tsx
+++ src/UserAuthentication/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input, Flex } from "antd";
+import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
   const onFinish = (values: any) => {
@@ -8,45 +9,56 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center mt-8">
-      <Form
-        name="login"
-        initialValues={{ remember: true }}
-        style={{ maxWidth: 360 }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Please input your Username!" }]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input
-            prefix={<LockOutlined />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Flex justify="space-between" align="center">
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-            <a href="">Forgot password</a>
-          </Flex>
-        </Form.Item>
+    <div className="flex flex-col items-center justify-center mt-16">
+      <div className="border rounded-md border-[#17295A] p-8">
+        <h1 className="mt-8 mb-8 text-center text-4xl font bold text-[#17295A]">
+          Sign in
+        </h1>
 
-        <Form.Item>
-          <Button block type="primary" htmlType="submit">
-            Log in
-          </Button>
-          or <a href="/registration">Register now!</a>
-        </Form.Item>
-      </Form>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          style={{ maxWidth: 400 }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              className="bg-[#17295A] text-white text-base px-1 py-2"
+              block
+              htmlType="submit"
+            >
+              Submit
+            </Button>
+            {/* or <a href="/registration">Register now!</a> */}
+            <div className="mt-4">
+              <Link to="/registration">
+                <p className="text-blue-700">
+                  <span className="text-gray-500">Not Registered? </span>
+                  Register now
+                </p>
+              </Link>
+              {/* <Button className="">Register Now</Button> */}
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
