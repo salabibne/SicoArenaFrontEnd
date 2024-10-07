da4d01ad4de79983a858c530a4e60bee2722467f
SALAB\dellv <salabyaham@gmail.com>
2024-10-07 22:05:16 +0600

    google authenticator has been concatenated with the application

---
+++ src/Store/useAuthStore.ts
@@ -0,0 +1,82 @@
+import { create } from "zustand";
+import {
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../src/firebase";
+
+const useAuthStore = create((set) => ({
+  user: null,
+  loading: true,
+  error: null,
+
+  // authListener
+
+  initializeAuthUser: () => {
+    set({ loading: true });
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        set({ user, loading: false });
+      } else {
+        set({ user: null, loading: false });
+      }
+    });
+  },
+
+  // createUser with Email And Password
+  signUp: async (email: string, password: string) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      set({ user: userCredential.user, loading: false, error: null });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        set({ error: error.message, loading: false });
+      } else {
+        set({ error: "An unknown error occurred", loading: false });
+      }
+      throw error;
+    }
+  },
+
+  // signIn with Email And Password
+  signIn: async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      set({ user: userCredential.user, loading: false, error: null });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        set({ error: error.message, loading: false });
+      } else {
+        set({ error: "An unknown error occurred", loading: false });
+      }
+      throw error;
+    }
+  },
+
+  // signOut
+  signOut: async () => {
+    try {
+      await signOut(auth);
+      set({ user: null, loading: false, error: null });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        set({ error: error.message, loading: false });
+      } else {
+        set({ error: "An unknown error occurred", loading: false });
+      }
+    }
+  },
+}));
+
+export default useAuthStore;
